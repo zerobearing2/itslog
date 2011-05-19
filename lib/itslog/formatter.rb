1b06ac4aaf7fe176ecd09670e79e42d246c5eedc
John Thomas Marino <writejm@gmail.com>
2011-05-19 13:51:14 -0400

    Add customizable colors; color by namespace; color by severity

--- lib/itslog/formatter.rb
+++ lib/itslog/formatter.rb
@@ -1,11 +1,27 @@
 module Itslog
   module Configure
     extend self
-    attr_accessor :format
+    attr_accessor :format, :namespace_colors, :severity_colors, :color_by
+
+    def color_by
+      @color_by ||= :severity
+    end
 
     def format
       @format ||= '%t %n %m'
     end
+
+    def namespace_colors
+      @namespace_colors ||= {
+        'action_controller' => "\e[32m",
+        'active_record'     => "\e[94m",
+        'action_view'       => "\e[36m"}
+    end
+
+    def severity_colors
+      @severity_colors ||= [ "\e[36m","\e[32m","\e[33m","\e[31m","\e[31m","\e[37m"]
+    end
+
   end
 end
 
@@ -22,17 +38,33 @@ module Itslog
     def add_with_format(severity, message = nil, progname = nil, &block)
       return if @level > severity
 
-      colors  = ["\e[36m","\e[32m","\e[33m","\e[31m","\e[31m","\e[37m"]
-      time    = Time.now.to_s(:db).split.last
-      message = colors[5] + message.to_s.strip
+      config = Itslog::Configure
 
-      msg = colors[severity] << Itslog::Configure.format.dup
+      format = config.format.dup
+      color  = color(namespace, severity)
+
+      time    = Time.now.to_s(:db).split.last
+      message = "\e[37m" + message.to_s.strip
+
+      msg = ''
+      msg << color if color.present?
+      msg << format
       msg.gsub!("%t", time)
       msg.gsub!("%n", namespace) if namespace.present?
       msg.gsub!("%m", message)
 
-      add_without_format \
-        severity, msg, progname, &block
+      add_without_format severity, msg, progname, &block
+    end
+
+    def color(namespace, severity)
+      color_by = Itslog::Configure.color_by
+      if color_by == :severity || severity > 1
+        Itslog::Configure.severity_colors[severity]
+      elsif color_by == :namespace
+        Itslog::Configure.namespace_colors[namespace]
+      else
+        raise 'color_by can only be :severity or :namespace'
+      end
     end
 
     included do
